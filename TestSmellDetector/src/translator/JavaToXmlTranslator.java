6b250704a9d400250b74f6da8825a4062810c77b
Antonio De Simone <antoniods311@gmail.com>
2018-01-09 17:36:00 +0100

    add class JavaToXmlTranslator.java

---
+++ TestSmellDetector/src/translator/JavaToXmlTranslator.java
@@ -0,0 +1,30 @@
+package translator;
+
+import java.io.File;
+
+public class JavaToXmlTranslator implements Translator {
+	
+	private File file;
+	
+	@Override
+	public String translate() {
+		// chiamata a tool di traduzione.
+		
+		return null;
+	}
+
+	@Override
+	public int load(File file) {
+		this.setFile(file);
+		return 0;
+	}
+
+	public File getFile() {
+		return file;
+	}
+
+	public void setFile(File file) {
+		this.file = file;
+	}
+
+}
