91abf804d123ed7f4ed27dc8603635b8f8e84c64
Antonio De Simone <antoniods311@gmail.com>
2018-01-15 21:18:20 +0100

    add method isTestMethod in EagerTestDetector; add contants in ToolConstant

--- TestSmellDetector/src/detector/EagerTestDetector.java
+++ TestSmellDetector/src/detector/EagerTestDetector.java
@@ -1,6 +1,18 @@
 package detector;
 
 import java.io.File;
+import java.io.IOException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.soap.Node;
+
+import util.ToolConstant;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 /**
  * 
@@ -10,20 +22,101 @@ import java.io.File;
 public class EagerTestDetector implements Detector{
 	
 	private File xml;
+	private DocumentBuilderFactory docbuilderFactory;
+	private DocumentBuilder documentBuilder;
+	private Document doc;
 	
 	public EagerTestDetector(File xml){
 		this.xml = xml;
 	}
 
+	/*
+	 * bisogna navigare ricorsivamente il doc xml in modo tale che per ogni function (annotata con @Test) che incontra
+	 * vado a verificare il contenuto dell'elemento block. 
+	 * Nel block ci possono essere 2 situazioni:
+	 * - trovo un assert e allora devo aggiungerlo alla lista di assert trovati;
+	 * - trovo un altro blocco. Allora devo fare chiamata ricorsiva per visitare il blocco.
+	 */
+	
+	/*
+	 * altra soluzione (per gestire anche casi in cui c'è un if annidato o un while per esempio):
+	 * - considero i singoli elementi function (annotati con @Test)
+	 * - mi prendo la lista di NODI
+	 * - scorro la lista prendendomi solo quelli che sono "ELEMENT_NODE"
+	 * - tra questi conto gli assert
+	 */
+	@Override
+	public int analyze() {
+
+		docbuilderFactory = DocumentBuilderFactory.newInstance();
+		try {
+			documentBuilder = docbuilderFactory.newDocumentBuilder();
+			doc = documentBuilder.parse(xml);
+			doc.getDocumentElement().normalize();
+			
+			System.out.println("Root:\t"+doc.getDocumentElement().getNodeName());
+			Element root = doc.getDocumentElement();
+			System.out.println("Language:\t"+root.getAttribute("language"));
+			
+			//leggo la lista di nodi function
+			NodeList list = doc.getElementsByTagName("function");
+			for(int i=0; i<list.getLength(); i++){
+				if(list.item(i).getNodeType() == Node.ELEMENT_NODE){
+					Element functionElement = (Element) list.item(i);
+					System.out.println(functionElement.getNodeName());
+					
+					if(isTestMethod(functionElement)){
+						System.out.println("ho trovato un test");
+					}
+					
+				}
+					
+					
+			}
+			
+			
+		} catch (ParserConfigurationException e) {
+			System.out.println(ToolConstant.PARSE_EXCEPTION_MSG);
+			e.printStackTrace();
+		} catch (SAXException e) {
+			System.out.println(ToolConstant.SAX_EXCEPTION_MSG);
+			e.printStackTrace();
+		} catch (IOException e) {
+			System.out.println(ToolConstant.IO_EXCEPTION_MSG);
+			e.printStackTrace();
+		}
+		
+		return 0;
+	}
+	
+	/*
+	 * metodo che controlla se un metodo del TC è un test o meno.
+	 * Si verifica che ci sia l'annotazione @Test
+	 */
+	private boolean isTestMethod(Element element) {
+
+		boolean isTest = false;
+		int i = 0;
+		NodeList annotationList = element.getElementsByTagName("annotation");
+		while(!isTest && i < annotationList.getLength()){
+			Element annotationElement = (Element) annotationList.item(i);
+			NodeList nameList = annotationElement.getElementsByTagName("name");
+			int j;
+			for(j=0; j<nameList.getLength(); j++){
+				Element nameElement = (Element) nameList.item(j);;
+				if(nameElement.getTextContent().equalsIgnoreCase(ToolConstant.TEST_ANNOTATION))
+					isTest = true;
+				j++;
+			}
+			i++;
+		}
+		
+		return isTest;
+	}
+
 	@Override
 	public void run(){
 		analyze();
-	}
-	
-	@Override
-	public int analyze() {
-		// TODO Auto-generated method stub
-		return 0;
 	}
 
 }
--- TestSmellDetector/src/util/ToolConstant.java
+++ TestSmellDetector/src/util/ToolConstant.java
@@ -8,11 +8,32 @@ public class ToolConstant {
 	public static final String XML_DIR = WORKING_DIR+"/outputXML/";
 	public static final String SRCML_DIR = WORKING_DIR+"/srcML/bin/";
 	
-	//Errors
-	public static final String SRCML_ERROR = "srcml tool execution error!";
-	
 	//Tool
 	public static final String SRCML_COMMAND = "srcml";
 	public static final String SRCML_OUTPUT_OPTION = "-o";
+	
+	//Tool Error
+	public static final String SRCML_ERROR = "srcml tool execution error!";
+	
+	//XML Parser Errors
+	public static final String PARSE_EXCEPTION_MSG = "ParserConfigurationException, document builder error!";
+	public static final String SAX_EXCEPTION_MSG = "SAX Exception, parse method error!";
+	public static final String IO_EXCEPTION_MSG = "IO Exception, parse method error!";
+	
+	//Annotations
+	public static final String TEST_ANNOTATION = "Test";
+	
+	//Assert Methods
+	public static final String[] ASSERT_METHODS = {
+			"fail",
+			"assertTrue",
+			"assertFalse",
+			"assertEquals",
+			"assertNull",
+			"assertNotNull",
+			"assertSame",
+			"assertNotSame"
+	};
+	
 	
 }
