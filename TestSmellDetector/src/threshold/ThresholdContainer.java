bf66c02c6a60f63fb4f949df6e8df3a6c9fec75a
Antonio De Simone <antoniods311@gmail.com>
2018-05-05 19:10:13 +0200

    implemented ThresholdContainer

--- TestSmellDetector/src/threshold/ThresholdContainer.java
+++ TestSmellDetector/src/threshold/ThresholdContainer.java
@@ -2,4 +2,225 @@ package threshold;
 
 public class ThresholdContainer {
 
+	//Values
+	private int assertionRouletteAbs;
+	private int eagerTestAbs;
+	private int indirectTestingAbs;
+	private int generalFixtureAbs;
+	private int mysteryGuestAbs;
+	private int sensitiveEqualityAbs;
+	private int codeDuplicationAbs;
+	private int lazyTestAbs;
+	
+	//Percentage
+	private double assertionRoulettePerc;
+	private double eagerTestPerc;
+	private double indirectTestingPerc;
+	private double generalFixturePerc;
+	private double mysteryGuestPerc;
+	private double sensitiveEqualityPerc;
+	private double codeDuplicationPerc;
+	private double lazyTestPerc;
+	
+	/**
+	 * Empty constructor
+	 */
+	public ThresholdContainer(){
+		
+	}
+	
+	/**
+	 * @return the assertionRouletteAbs
+	 */
+	public int getAssertionRouletteAbs() {
+		return assertionRouletteAbs;
+	}
+	/**
+	 * @param assertionRouletteAbs the assertionRouletteAbs to set
+	 */
+	public void setAssertionRouletteAbs(int assertionRouletteAbs) {
+		this.assertionRouletteAbs = assertionRouletteAbs;
+	}
+	/**
+	 * @return the eagerTestAbs
+	 */
+	public int getEagerTestAbs() {
+		return eagerTestAbs;
+	}
+	/**
+	 * @param eagerTestAbs the eagerTestAbs to set
+	 */
+	public void setEagerTestAbs(int eagerTestAbs) {
+		this.eagerTestAbs = eagerTestAbs;
+	}
+	/**
+	 * @return the indirectTestingAbs
+	 */
+	public int getIndirectTestingAbs() {
+		return indirectTestingAbs;
+	}
+	/**
+	 * @param indirectTestingAbs the indirectTestingAbs to set
+	 */
+	public void setIndirectTestingAbs(int indirectTestingAbs) {
+		this.indirectTestingAbs = indirectTestingAbs;
+	}
+	/**
+	 * @return the generalFixtureAbs
+	 */
+	public int getGeneralFixtureAbs() {
+		return generalFixtureAbs;
+	}
+	/**
+	 * @param generalFixtureAbs the generalFixtureAbs to set
+	 */
+	public void setGeneralFixtureAbs(int generalFixtureAbs) {
+		this.generalFixtureAbs = generalFixtureAbs;
+	}
+	/**
+	 * @return the mysteryGuestAbs
+	 */
+	public int getMysteryGuestAbs() {
+		return mysteryGuestAbs;
+	}
+	/**
+	 * @param mysteryGuestAbs the mysteryGuestAbs to set
+	 */
+	public void setMysteryGuestAbs(int mysteryGuestAbs) {
+		this.mysteryGuestAbs = mysteryGuestAbs;
+	}
+	/**
+	 * @return the sensitiveEqualityAbs
+	 */
+	public int getSensitiveEqualityAbs() {
+		return sensitiveEqualityAbs;
+	}
+	/**
+	 * @param sensitiveEqualityAbs the sensitiveEqualityAbs to set
+	 */
+	public void setSensitiveEqualityAbs(int sensitiveEqualityAbs) {
+		this.sensitiveEqualityAbs = sensitiveEqualityAbs;
+	}
+	/**
+	 * @return the codeDuplicationAbs
+	 */
+	public int getCodeDuplicationAbs() {
+		return codeDuplicationAbs;
+	}
+	/**
+	 * @param codeDuplicationAbs the codeDuplicationAbs to set
+	 */
+	public void setCodeDuplicationAbs(int codeDuplicationAbs) {
+		this.codeDuplicationAbs = codeDuplicationAbs;
+	}
+	/**
+	 * @return the lazyTestAbs
+	 */
+	public int getLazyTestAbs() {
+		return lazyTestAbs;
+	}
+	/**
+	 * @param lazyTestAbs the lazyTestAbs to set
+	 */
+	public void setLazyTestAbs(int lazyTestAbs) {
+		this.lazyTestAbs = lazyTestAbs;
+	}
+	/**
+	 * @return the assertionRoulettePerc
+	 */
+	public double getAssertionRoulettePerc() {
+		return assertionRoulettePerc;
+	}
+	/**
+	 * @param assertionRoulettePerc the assertionRoulettePerc to set
+	 */
+	public void setAssertionRoulettePerc(double assertionRoulettePerc) {
+		this.assertionRoulettePerc = assertionRoulettePerc;
+	}
+	/**
+	 * @return the eagerTestPerc
+	 */
+	public double getEagerTestPerc() {
+		return eagerTestPerc;
+	}
+	/**
+	 * @param eagerTestPerc the eagerTestPerc to set
+	 */
+	public void setEagerTestPerc(double eagerTestPerc) {
+		this.eagerTestPerc = eagerTestPerc;
+	}
+	/**
+	 * @return the indirectTestingPerc
+	 */
+	public double getIndirectTestingPerc() {
+		return indirectTestingPerc;
+	}
+	/**
+	 * @param indirectTestingPerc the indirectTestingPerc to set
+	 */
+	public void setIndirectTestingPerc(double indirectTestingPerc) {
+		this.indirectTestingPerc = indirectTestingPerc;
+	}
+	/**
+	 * @return the generalFixturePerc
+	 */
+	public double getGeneralFixturePerc() {
+		return generalFixturePerc;
+	}
+	/**
+	 * @param generalFixturePerc the generalFixturePerc to set
+	 */
+	public void setGeneralFixturePerc(double generalFixturePerc) {
+		this.generalFixturePerc = generalFixturePerc;
+	}
+	/**
+	 * @return the mysteryGuestPerc
+	 */
+	public double getMysteryGuestPerc() {
+		return mysteryGuestPerc;
+	}
+	/**
+	 * @param mysteryGuestPerc the mysteryGuestPerc to set
+	 */
+	public void setMysteryGuestPerc(double mysteryGuestPerc) {
+		this.mysteryGuestPerc = mysteryGuestPerc;
+	}
+	/**
+	 * @return the sensitiveEqualityPerc
+	 */
+	public double getSensitiveEqualityPerc() {
+		return sensitiveEqualityPerc;
+	}
+	/**
+	 * @param sensitiveEqualityPerc the sensitiveEqualityPerc to set
+	 */
+	public void setSensitiveEqualityPerc(double sensitiveEqualityPerc) {
+		this.sensitiveEqualityPerc = sensitiveEqualityPerc;
+	}
+	/**
+	 * @return the codeDuplicationPerc
+	 */
+	public double getCodeDuplicationPerc() {
+		return codeDuplicationPerc;
+	}
+	/**
+	 * @param codeDuplicationPerc the codeDuplicationPerc to set
+	 */
+	public void setCodeDuplicationPerc(double codeDuplicationPerc) {
+		this.codeDuplicationPerc = codeDuplicationPerc;
+	}
+	/**
+	 * @return the lazyTestPerc
+	 */
+	public double getLazyTestPerc() {
+		return lazyTestPerc;
+	}
+	/**
+	 * @param lazyTestPerc the lazyTestPerc to set
+	 */
+	public void setLazyTestPerc(double lazyTestPerc) {
+		this.lazyTestPerc = lazyTestPerc;
+	}
+	
+	
 }
